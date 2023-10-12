333f4b6bb234a64181bf2115a89f2127daff77c4
Sean <sinbad4u007@yahoo.com>
2023-10-12 05:31:36 -0700

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,57 +1,50 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
-
 /**
- * print_all - print any arguments provided to a function
- * @format: the argument specifier
- *
- * Return: void
- */
+* print_all - print any arguments provided to a function
+* @format: the argument specifier
+*
+* Return: any arguments based on specified formart
+*/
 void print_all(const char * const format, ...)
 {
-    int i = 0;
-    char *str;
-    va_list spc;
-    int check_stat = 0; // Initialize check_stat to 0
+	int i; /* declare variables and var_arg datatype */
+	int check_stat = 0;
+	char *str;
+	va_list spc;
 
-    va_start(spc, format);
+	va_start(spc, format); /* initialize var arguments */
 
-    while (format != NULL && format[i] != '\0')
-    {
-        switch (format[i])
-        {
-            case 'i':
-                printf("%d", va_arg(spc, int));
-                check_stat = 0;
-                break;
-            case 'f':
-                printf("%f", va_arg(spc, double));
-                check_stat = 0;
-                break;
-            case 'c':
-                printf("%c", (char) va_arg(spc, int)); // Cast to char
-                check_stat = 0;
-                break;
-            case 's':
-                str = va_arg(spc, char *);
-                if (str == NULL)
-                    str = "(nil)";
-                printf("%s", str);
-                check_stat = 0;
-                break;
-            default:
-                check_stat = 1;
-                break;
-        }
-
-        if (format[i + 1] != '\0' && check_stat == 0)
-            printf(", ");
-
-        i++;
-    }
-
-    va_end(spc);
-    printf("\n");
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'i':
+		printf("%d", va_arg(spc, int));
+		check_stat = 0; /* check if condition has been met */
+		break;
+		case 'f':
+			printf("%f", va_arg(spc, double));
+			check_stat = 0;
+			break;
+		case 'c':
+			printf("%c", (char) va_arg(spc, int));
+			check_stat = 0;
+			break;
+		case 's':
+			str = va_arg(spc, char *);
+				if (str == NULL)
+				str = "(nil)";
+					printf("%s", str);
+				break;
+		default:
+			check_stat = 1;
+			break;
+		}
+		if (format[i + 1] != '\0' && check_stat == 0) /* if NOT NULL */
+			printf(", ");
+		i++; /* update step of iteration of var */
+	}
+	printf("\n");
+	va_end(spc); /* end traversal */
 }
-
